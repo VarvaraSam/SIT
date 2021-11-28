b447323146479dbf45738eaf7dea5198dda30071
VarvaraSam <91589476+VarvaraSam@users.noreply.github.com>
2021-11-28 15:28:10 +1000

    Add files via upload

--- npm/index.js
+++ npm/index.js
@@ -1,1 +1,6 @@
-
+import chalk from 'chalk';
+console.log(chalk.redBright("Привет всем!!"));
+console.log(chalk.bgBlue("Один интересный факт про chalk:"));
+console.log(chalk.overline("Он был первым пакетом в списке популярных пакетов"));
+console.log(chalk.bgCyan("Я решила долго не искать и использовать его"));
+console.log(chalk.overline.yellowBright("Да я оригинальная)"));
