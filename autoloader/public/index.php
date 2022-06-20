c166aa31aeb4deb377fba461a9a7a5f37933ca6e
VarvaraSam <91589476+VarvaraSam@users.noreply.github.com>
2022-06-20 12:00:17 +1000

    Add files via upload

--- autoloader/public/index.php
+++ autoloader/public/index.php
@@ -1,1 +1,15 @@
+<?php
+spl_autoload_register(function ($class_name) {
+    $class = str_replace("\\", "/", $class_name);
+    $root= dirname(__DIR__) . "/src/{$class}.php";
+    require $root;
 
+});
+use subdir\Class2;
+
+$cl1 = new Class1();
+$cl2 = new Class2();
+
+$cl1->Class1Call();
+
+$cl2->Class2Call();
