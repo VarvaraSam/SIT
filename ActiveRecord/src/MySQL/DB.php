2a6cc1d7ffb809b1542e13879f386982f2dd2b59
VarvaraSam <91589476+VarvaraSam@users.noreply.github.com>
2022-06-22 02:41:08 +1000

    Add files via upload

--- ActiveRecord/src/MySQL/DB.php
+++ ActiveRecord/src/MySQL/DB.php
@@ -1,1 +1,35 @@
+<?php
 
+namespace Meme\Chat\MySQL;
+
+use PDO;
+
+class DB
+{
+    private $l;
+
+    public function __construct()
+    {
+        $this->l = new PDO('mysql:host=localhost;dbname=sit', 'meme', 'memepass');
+    }
+
+
+    public function execute($sql)
+    {
+        $sth = $this->l->prepare($sql);
+        return $sth->execute();
+    }
+
+    public function query($sql): array
+    {
+        $sth = $this->l->prepare($sql);
+        $sth->execute();
+
+        $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($result === false)
+            return [];
+
+        return  $result;
+    }
+}
