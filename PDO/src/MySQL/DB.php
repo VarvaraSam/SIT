9897188e86471c89d5b810fccfdda13952e31aec
VarvaraSam <91589476+VarvaraSam@users.noreply.github.com>
2022-06-21 17:27:23 +1000

    Add files via upload

--- PDO/src/MySQL/DB.php
+++ PDO/src/MySQL/DB.php
@@ -1,1 +1,39 @@
+<?php
 
+namespace Meme\Chat\MySQL;
+
+use PDO;
+
+class DB
+{
+    private $link;
+
+    public function __construct()
+    {
+        $this->connect();
+    }
+
+    private function connect(): void
+    {
+        $this->link = new PDO('mysql:host=localhost;dbname=sit', 'meme', 'memepass');
+    }
+
+    public function execute($sql)
+    {
+        $sth = $this->link->prepare($sql);
+        return $sth->execute();
+    }
+
+    public function query($sql): array
+    {
+        $sth = $this->link->prepare($sql);
+        $sth->execute();
+
+        $result = $sth->fetchAll(PDO::FETCH_ASSOC);
+
+        if ($result === false)
+            return [];
+
+        return  $result;
+    }
+}
