0dd95097e0ec8b30e65879f714b5c0c328eead6a
VarvaraSam <91589476+VarvaraSam@users.noreply.github.com>
2022-06-22 09:40:34 +1000

    Update DB.php

--- PDO/src/MySQL/DB.php
+++ PDO/src/MySQL/DB.php
@@ -10,30 +10,15 @@ class DB
 
     public function __construct()
     {
-        $this->connect();
-    }
-
-    private function connect(): void
-    {
         $this->link = new PDO('mysql:host=localhost;dbname=sit', 'meme', 'memepass');
     }
 
+    
     public function execute($sql)
     {
         $sth = $this->link->prepare($sql);
         return $sth->execute();
     }
 
-    public function query($sql): array
-    {
-        $sth = $this->link->prepare($sql);
-        $sth->execute();
-
-        $result = $sth->fetchAll(PDO::FETCH_ASSOC);
-
-        if ($result === false)
-            return [];
-
-        return  $result;
-    }
+    
 }
