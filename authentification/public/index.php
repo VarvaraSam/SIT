deb25e60e7c6c4d47e166878f8fe7d5fb6537821
VarvaraSam <91589476+VarvaraSam@users.noreply.github.com>
2022-06-24 02:46:58 +1000

    Add files via upload

--- authentification/public/index.php
+++ authentification/public/index.php
@@ -1,1 +1,19 @@
+<?php
+
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
+use Meme\Authentification\Controller\Controller;
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+$loader = new FilesystemLoader(dirname(__DIR__) . "/src/View/");
+$twig = new Environment($loader);
+$controller = new Controller($twig);
+
+//<button name = "regbtn" onclick="window.location.href = 'http://46.101.239.77:82/registration'">Регистрация</button>
+$controller->control();
+//$uri = $_SERVER['REQUEST_URI'];
+//if (isset($_COOKIE['Reglogin']))
+//{
+//    echo "Пользователь зарегестрирован </p>";
+//}
+
 
