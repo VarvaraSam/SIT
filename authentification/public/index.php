d89fcabfa3f3191ad4a5f11af24ee6123a19af3f
VarvaraSam <91589476+VarvaraSam@users.noreply.github.com>
2022-06-24 12:14:32 +1000

    Add files via upload

--- authentification/public/index.php
+++ authentification/public/index.php
@@ -8,12 +8,8 @@ $loader = new FilesystemLoader(dirname(__DIR__) . "/src/View/");
 $twig = new Environment($loader);
 $controller = new Controller($twig);
 
-//<button name = "regbtn" onclick="window.location.href = 'http://46.101.239.77:82/registration'">Регистрация</button>
-$controller->control();
 //$uri = $_SERVER['REQUEST_URI'];
-//if (isset($_COOKIE['Reglogin']))
-//{
-//    echo "Пользователь зарегестрирован </p>";
-//}
 
 
+
+$controller->control();
